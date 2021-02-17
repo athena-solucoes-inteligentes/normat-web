7ae1c66b759d2b00df9fe6016ced80eac4cdfc1b
Pedro Campos <pedro@cdias.dev>
2021-02-17 18:11:59 -0300

    Update api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://athena-improver.herokuapp.com';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://athena-normat.herokuapp.com';
 
 export default axios.create({
   baseURL,
